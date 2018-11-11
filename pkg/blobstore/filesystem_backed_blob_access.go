283644b1687f6dbe9edb8bbc355dc289fc189e87
Ed Schouten <ed.schouten@prodrive-technologies.com>
2018-11-11 20:27:43 +0100

    XXX.

--- pkg/blobstore/filesystem_backed_blob_access.go
+++ pkg/blobstore/filesystem_backed_blob_access.go
@@ -1,12 +1,15 @@
 package blobstore
 
 import (
+	"bytes"
 	"context"
 	"crypto/sha256"
 	"encoding/binary"
 	"io"
 	"io/ioutil"
 	"log"
+	"strconv"
+	"strings"
 	"sync"
 
 	"github.com/EdSchouten/bazel-buildbarn/pkg/util"
@@ -30,10 +33,13 @@ func newRawDigest(digest *util.Digest) rawDigest {
 	return r
 }
 
+type offsetEntry [len(rawDigest{}) + 8]byte
+
 type partialTable struct {
-	offsets     map[rawDigest]int64
-	data        readWriterAt
-	writeOffset int64
+	offsets map[rawDigest]int64
+
+	data     readWriterAt
+	dataSize int64
 }
 
 func (t *partialTable) getBlobOffset(digest rawDigest) (int64, bool) {
@@ -42,28 +48,64 @@ func (t *partialTable) getBlobOffset(digest rawDigest) (int64, bool) {
 }
 
 func (t *partialTable) getBlobReader(offset int64, length int64) io.ReadCloser {
-	return ioutil.NopCloser(io.NewSectionReader(t.data, offset, int64(length)))
+	return ioutil.NopCloser(io.NewSectionReader(t.data, offset, length))
 }
 
 func (t *partialTable) putBlob(digest rawDigest, data []byte) error {
-	n, err := t.data.WriteAt(data, int64(t.writeOffset))
+	n, err := t.data.WriteAt(data, t.dataSize)
 	if err == nil {
 		return err
 	}
-	t.offsets[digest] = t.writeOffset
-	t.writeOffset += int64(n)
+	t.offsets[digest] = t.dataSize
+	t.dataSize += int64(n)
 	return nil
 }
 
 type completeTable struct {
+	bloomFilter       io.ReaderAt
+	bloomFilterSize   int64
+	bloomFilterRounds int
+
+	offsets                  io.ReaderAt
+	offsetsSize              int64
+	offsetsMaximumIterations int
+
 	data io.ReaderAt
 }
 
 func (t *completeTable) matchesBloomFilter(digest rawDigest) (bool, error) {
-	return false, nil
+	for i := 0; i < t.bloomFilterRounds; i++ {
+		hash := binary.LittleEndian.Uint32(digest[i*4 : (i+1)*4])
+		offsetByte := int64(hash/8) % t.bloomFilterSize
+		offsetBit := hash % 8
+		var b [1]byte
+		if _, err := t.bloomFilter.ReadAt(b[:], offsetByte); err == io.EOF {
+			// Malformed bloom filter.
+			return false, nil
+		} else if err != nil {
+			// I/O error.
+			return false, err
+		} else if b[0]&(1<<offsetBit) == 0 {
+			// Digest not part of bloom filter.
+			return false, nil
+		}
+	}
+	return true, nil
 }
 
 func (t *completeTable) getBlobOffset(digest rawDigest) (int64, bool, error) {
+	slots := t.offsetsSize / int64(len(offsetEntry{}))
+	hash := binary.LittleEndian.Uint32(digest[0:4])
+	for i := 0; i < t.offsetsMaximumIterations; i++ {
+		var offsetEntry offsetEntry
+		if _, err := t.offsets.ReadAt(offsetEntry[:], (int64(hash)%slots)*int64(len(offsetEntry))); err != nil {
+			return 0, false, err
+		}
+		if bytes.Equal(digest[:], offsetEntry[:len(digest)]) {
+			return int64(binary.LittleEndian.Uint64(offsetEntry[len(digest):])), true, nil
+		}
+		hash += uint32(i) + 1
+	}
 	return 0, false, nil
 }
 
@@ -83,8 +125,40 @@ type filesystemBackedBlobAccess struct {
 	completeTables []completeTable
 }
 
-func NewFilesytemBackedBlobAccess() BlobAccess {
-	return &filesystemBackedBlobAccess{}
+type FilesystemAccess interface {
+	ReadDir() ([]string, error)
+	OpenReadMapped(filename string) (io.ReaderAt, error)
+}
+
+func NewFilesystemBackedBlobAccess(filesystemAccess FilesystemAccess) (BlobAccess, error) {
+	entries, err := filesystemAccess.ReadDir()
+	if err != nil {
+		return nil, err
+	}
+
+	// Scan the directory to see which data is already there.
+	var firstFreeIndex uint
+	for _, entry := range entries {
+		if !strings.HasSuffix(entry, ".index") {
+			continue
+		}
+
+		// Compute first free index number for next data file.
+		if index, err := strconv.ParseUint(strings.TrimSuffix(entry, ".index"), 10, 0); err != nil {
+			log.Printf("Failed extract index number from file %s", entry)
+			continue
+		} else if firstFreeIndex <= uint(index) {
+			firstFreeIndex = uint(index) + 1
+		}
+
+		_, err := filesystemAccess.OpenReadMapped(entry)
+		if err != nil {
+			log.Printf("Failed to open index file %s: %s", entry, err)
+			continue
+		}
+	}
+
+	return &filesystemBackedBlobAccess{}, nil
 }
 
 func (ba *filesystemBackedBlobAccess) Get(ctx context.Context, digest *util.Digest) io.ReadCloser {
