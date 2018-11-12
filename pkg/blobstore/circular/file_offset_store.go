c36cc6546934f0dbb1d6ab8739d8f97791812d87
Ed Schouten <ed.schouten@prodrive-technologies.com>
2018-11-12 20:56:29 +0100

    XXX.

--- pkg/blobstore/circular/file_offset_store.go
+++ pkg/blobstore/circular/file_offset_store.go
@@ -10,38 +10,53 @@ import (
 	"github.com/EdSchouten/bazel-buildbarn/pkg/util"
 )
 
+const (
+	slotsPerKey = 10
+	maximumIterationsPerPut = 100
+)
+
 type offsetKey [sha256.Size + 4 + 4 + 8]byte
 
-func newOffsetKey(digest *util.Digest, attempt int, offset uint64) offsetKey {
+func newOffsetKey(digest *util.Digest, offset uint64) offsetKey {
 	var offsetKey offsetKey
 	copy(offsetKey[:sha256.Size], digest.GetHash())
 	binary.LittleEndian.PutUint32(offsetKey[sha256.Size:], uint32(digest.GetSizeBytes()))
-	binary.LittleEndian.PutUint32(offsetKey[sha256.Size+4:], uint32(attempt))
 	binary.LittleEndian.PutUint64(offsetKey[sha256.Size+8:], offset)
 	return offsetKey
 }
 
 type slotsList [sha256.Size / 4]uint32
 
-func (ok *offsetKey) getSlotsList() slotsList {
-	var slotsList slotsList
-	for i := 0; i < len(slotsList); i++ {
-		slotsList[i] = binary.LittleEndian.Uint32(ok[i*4:])
+func (ok *offsetKey) getSlot() uint32 {
+	slot := uint32(2166136261)
+	for _, b := range ok[:sha256.Size + 8] {
+		slot ^= uint32(b)
+		slot *= 16777619
 	}
-	return slotsList
+	return slot
+}
+
+func (ok *offsetKey) getAttempt() uint32 {
+	return binary.LittleEndian.Uint32(ok[sha256.Size+4:])
 }
 
 func (ok *offsetKey) getOffset() uint64 {
 	return binary.LittleEndian.Uint64(ok[sha256.Size+8:])
 }
 
-func (ok *offsetKey) digestEqual(other offsetKey) bool {
+func (ok *offsetKey) digestAndAttemptEqual(other offsetKey) bool {
 	return bytes.Equal(ok[:sha256.Size+8], other[:sha256.Size+8])
 }
 
 func (ok *offsetKey) offsetInBounds(minOffset uint64, maxOffset uint64) bool {
 	offset := ok.getOffset()
 	return offset >= minOffset || offset <= maxOffset
+}
+
+func (ok *offsetKey) withAttempt(attempt uint32) offsetKey {
+	newKey := *ok
+	binary.LittleEndian.PutUint32(newKey[sha256.Size+4:], attempt)
+	return newKey
 }
 
 type fileOffsetStore struct {
@@ -79,8 +94,10 @@ func (os *fileOffsetStore) putKeyAtPosition(key offsetKey, position int64) error
 }
 
 func (os *fileOffsetStore) Get(digest *util.Digest, minOffset uint64, maxOffset uint64) (uint64, bool, error) {
-	lookupKey := newOffsetKey(digest, 0, 0)
-	for attempt, slot := range lookupKey.getSlotsList() {
+	key := newOffsetKey(digest, 0)
+	for attempt := uint32(0); attempt < slotsPerKey; attempt++ {
+		lookupKey := key.withAttempt(attempt)
+		slot := lookupKey.getSlot()
 		position := os.getPositionOfSlot(slot)
 		storedKey, err := os.getKeyAtPosition(position)
 		if err != nil {
@@ -89,38 +106,51 @@ func (os *fileOffsetStore) Get(digest *util.Digest, minOffset uint64, maxOffset 
 		if !storedKey.offsetInBounds(minOffset, maxOffset) {
 			break
 		}
-		if storedKey.digestEqual(lookupKey) && storedKey.attemptEqual(attempt) {
+		if storedKey.digestAndAttemptEqual(lookupKey) {
 			return storedKey.getOffset(), true, nil
 		}
 	}
 	return 0, false, nil
 }
 
-func (os *fileOffsetStore) pushKeyToNextSlot(key offsetKey, minOffset uint64, maxOffset uint64) error {
-	if offset := key.getOffset(); offset < minOffset || offset > maxOffset {
-		return nil
+func (os *fileOffsetStore) putKey(key offsetKey, minOffset uint64, maxOffset uint64) (offsetKey, bool, error) {
+	slot := key.getSlot()
+	position := os.getPositionOfSlot(slot)
+
+	// Fetch the old key. If it is invalid, or already at a spot where it
+	// can't be moved to another place, simply overwrite it.
+	oldKey, err := os.getKeyAtPosition(position)
+	if err != nil {
+		return offsetKey{}, false, err
 	}
-	slotsList := key.getSlotsList()
-	currentAttempt := key.getAttempt()
-	if currentAttempt < 0 || currentAttempt >= len(slotsList) ||
-		os.getPositionOfSlot(slotsList[currentAttempt]) != position {
-		return nil
+	oldAttempt := oldKey.getAttempt()
+	if !oldKey.offsetInBounds(minOffset, maxOffset) ||
+		oldAttempt >= slotsPerKey - 1 ||
+		os.getPositionOfSlot(oldKey.getSlot()) != position {
+		return offsetKey{}, false, os.putKeyAtPosition(key, position)
 	}
 
-	for attempt := oldAttempt + 1; attempt < len(slotsList); attempt++ {
+	if oldKey.getOffset() <= key.getOffset() {
+		return oldKey.withAttempt(oldAttempt + 1), true, os.putKeyAtPosition(key, position)
 	}
+
+	attempt := key.getAttempt()
+	if attempt >= slotsPerKey - 1 {
+		return offsetKey{}, false, nil
+	}
+	return key.withAttempt(attempt + 1), true, nil
 }
 
 func (os *fileOffsetStore) Put(digest *util.Digest, minOffset uint64, newOffset uint64) error {
 	key := newOffsetKey(digest, newOffset)
-	slotsList := key.getSlotsList()
-	position := os.getPositionOfSlot(slotsList[0])
-	oldKey, err := os.getKeyAtPosition(position)
-	if err != nil {
-		return err
+	for i := 0; i < maximumIterationsPerPut; i++ {
+		if nextKey, more, err := os.putKey(key, minOffset, newOffset); err != nil {
+			return err
+		} else if more {
+			key = nextKey
+		} else {
+			break
+		}
 	}
-	if err := os.putKeyAtPosition(key, position); err != nil {
-		return err
-	}
-	return os.pushKeyToNextSlot(oldKey, minOffset, newOffset)
+	return nil
 }
